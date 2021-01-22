41d6f73fbcecf4a338a6f448323722d39138496c
Melek OLCAY <39502040+melekolcayy@users.noreply.github.com>
2021-01-22 23:44:50 +0300

    first commit

---
+++ MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+    class MyDictionary<K, V>
+    {
+        public K[] keys;
+        public V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[tempKeys.Length + 1];
+            values = new V[tempValues.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            values[values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> makyajMalzemeleri = new MyDictionary<int,string>();
+            makyajMalzemeleri.Add(40, "Highlighter");
+            makyajMalzemeleri.Add(100, "Fondöten");
+            makyajMalzemeleri.Add(60, "Maskara");
+            Console.WriteLine("Makyaj Malzemelerim :" + makyajMalzemeleri.Count);
+        }
+    }
+}
